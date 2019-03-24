37451af531046629ceeefad8bc9f93993ac62c15
y <y@ru.net>
2019-03-24 17:59:26 +0300

    DZ1 complete v1.03

--- app/src/main/java/com/example/dz1/ListFragment.java
+++ app/src/main/java/com/example/dz1/ListFragment.java
@@ -23,7 +23,7 @@ public class ListFragment extends Fragment implements DataAdapter.ItemClickListe
     View rootView;
 
     void setItems(ArrayList<Integer> arLst){
-        for (int i = 0; i<100; i++){
+        for (int i = 1; i<101; i++){
             arLst.add(i);
         }
     }
